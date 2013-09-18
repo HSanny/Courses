c71692c4d54f8cad5e8dfd62dd2a893af32c5458
JimmyLin192 <linxin@gmail.com>
2013-09-18 01:09:25 -0500

    update

--- Projects/p01/msh.c
+++ Projects/p01/msh.c
@@ -347,20 +347,15 @@ void sigchld_handler(int sig)
 void sigint_handler(int sig) 
 {
     int i;
-    if (kill(-1 * getpid(), SIGINT)) {
-        printf("Error Signal Delivery.. SIGINT\n");
-        exit(-3);
-    }
+
     // print information to screen
     for (i = 0; i < MAXJOBS; i ++) {
         if (jobs[i].state == FG) {
-            /*
             // send the signal SIGINT to foreground job
             if (kill(jobs[i].pid, SIGINT)) {
                 printf("\n Signal Delivery Failure. \n");  
                 exit(-3);
             }
-            */
             printf("\n Job [%d] (%d) terminated by signal %d \n", 
                     jobs[i].jid, jobs[i].pid, SIGINT);
             deletejob(jobs, jobs[i].pid);
@@ -377,20 +372,14 @@ void sigint_handler(int sig)
 void sigtstp_handler(int sig) 
 {
     int i;
-    if (kill(-1 * getpid(), SIGTSTP)) {
-        printf("Error Signal Delivery.. SIGINT\n");
-        exit(-3);
-    }
     // print information to screen
     for (i = 0; i < MAXJOBS; i ++) {
         if (jobs[i].state == FG) {
-            /*
             // send the signal SIGINT to foreground job
              if (kill(jobs[i].pid, SIGTSTP)) {
                 printf("\n Signal Delivery Failure. \n");  
                 exit(-3);
             }
-            */
             printf("\n Job [%d] (%d) stopped by signal %d \n", 
                     jobs[i].jid, jobs[i].pid, SIGTSTP);
         }

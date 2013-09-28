e699d7b0b589147c33f7042762f3fc2f350b5680
jimmylin192 <jimmylin@cs.utexas.edu>
2013-09-27 19:21:23 -0500

    before priority scheduling

--- Projects/p02/src/devices/timer.c
+++ Projects/p02/src/devices/timer.c
@@ -118,7 +118,6 @@ timer_sleep (int64_t ticks)
 {
     ASSERT (intr_get_level () == INTR_ON);
 
-    // TODO: remove the busy waiting, replaced by the ready queue mechanism
     // -------------------------------------------------------
     // Original implementation
     // int64_t start = timer_ticks ();

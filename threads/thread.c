e9893289e5d9090d3235aa09d6aff0f5728fd0b3
jimmylin192 <jimmylin@cs.utexas.edu>
2013-10-19 17:04:43 -0500

    thread.c upload

--- threads/thread.c
+++ threads/thread.c
@@ -198,6 +198,9 @@ thread_create (const char *name, int priority,
   /* Initialize thread. */
   init_thread (t, name, priority);
   tid = t->tid = allocate_tid ();
+  //****************************************************
+  t->parent = thread_current()->tid;
+  //****************************************************
 
   /* Prepare thread for first run by initializing its stack.
      Do this atomically so intermediate values for the 'stack' 

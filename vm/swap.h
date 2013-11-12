f4d821ec32700e8181b0398462341610d7d78cca
linxin@gmail.com <linxin@gmail.com>
2013-11-12 03:59:29 -0600

    swap design

--- vm/swap.h
+++ vm/swap.h
@@ -1,13 +1,22 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+#include "threads/synch.h"
+#include "vm/frame.h"
 
+struct swap_pool {
+    struct lock lock;
+    size_t swap_num_slots;
+    struct bitmap * swap_map;
+}
 
+void swap_init (void);
 
+void * swap_get_frame (struct frame * evict);
 
+bool swap_out (struct FTE * fte, bool dirty);
 
-
-
+struct FTE * read_from_swap (struct page * fault_page);
 
 
 #endif /* vm/swap.h */

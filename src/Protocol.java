6defba0422271bed9b364c7e1f4e5e25eb9104d1
Calvin Szeto <szeto.calvin@gmail.com>
2014-04-03 15:32:28 -0500

    Finish send decision in Commander.

--- src/Protocol.java
+++ src/Protocol.java
@@ -49,11 +49,13 @@ interface Protocol {
     final static String RESPONSE_CONTENT = "%d" + CONTENT_SEP + "%s" + CONTENT_SEP + "%d";
     final static String ADOPTED_CONTENT = "%d" + CONTENT_SEP + "%s";
     final static String PREEMPTED_CONTENT = "%d";
+    final static String DECISION_CONTENT = "%d" + CONTENT_SEP + "%s";
 
     final static String ACCEPTED_SEP = "/a";
     final static String PVALUE_SEP = "/p";
     final static String P1A_CONTENT = "%d" + CONTENT_SEP + "%d";
     final static String P1B_CONTENT = "%d" + CONTENT_SEP + "%d" + CONTENT_SEP + "%s";
+    final static String P2A_CONTENT = "%d" + CONTENT_SEP + "%d";
     final static String P2B_CONTENT = "%d" + CONTENT_SEP + "%d";
 
     // message design

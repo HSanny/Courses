09a6c50cca9375e327816a9284267d31ffc723d1
Calvin Szeto <szeto.calvin@gmail.com>
2014-04-03 17:27:42 -0500

    Change Acceptor p2b message to ACCEPTOR_TYPE.

--- src/Acceptor.java
+++ src/Acceptor.java
@@ -105,7 +105,7 @@ class Acceptor extends Util implements Runnable {
                     // send a p2b in response with the current ballot number
                     int port = SERVER_PORT_BASE + lambda;
                     String p2b_content = String.format(P2B_CONTENT, serverID, ballot_num);
-                    String p2b_response = String.format(MESSAGE, SERVER_TYPE, serverID, LEADER_TYPE, lambda, 
+                    String p2b_response = String.format(MESSAGE, ACCEPTOR_TYPE, serverID, LEADER_TYPE, lambda, 
                            P2B_TITLE, p2b_content);
                     try {
                     send (localhost, port, p2b_response, logHeader);

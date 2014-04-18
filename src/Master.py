f89d9349593f4fc108a72b594cdee4ca311588e0
jimmylin192 <linxin@gmail.com>
2014-04-17 21:03:00 -0500

    project description up and master program update

--- src/Master.py
+++ src/Master.py
@@ -51,15 +51,32 @@ def MasterListener(log):
 
 
 def MasterProcessor(log):
-    for command in sys.stdin:
-        args = command.split(" ")
-        cmd = args[0]
-        print cmd
-        if cmd == "startClient":
-            ''' 
-            startClient(i,j): Start a client with client id i, which is
-            conneted to server j.
-            ''' 
+    for line in fileinput.input():
+        line = line.split()
+        if line[0] ==  "joinServer":
+            serverId = int(line[1])
+            """
+            Start up a new server with this id and connect it to all servers
+            """
+            assert (serverIdx not in allServers), \
+                    "JOIN: server %d already exists." % serverIdx
+            allServers.update([serverIdx])
+
+            print "join ", serverIdx, "completes."
+
+        if line[0] ==  "retireServer":
+            serverId = int(line[1])
+            """
+            Retire the server with the id specified. This should block until
+            the server can tell another server of its retirement
+            """ 
+        if line[0] ==  "joinClient":
+            clientId = int(line[1])
+            serverId = int(line[2])
+            """
+            Start a new client with the id specified and connect it to 
+            the server
+            """
             clientIdx = int (args[1])
             serverIdx = int (args[2])
             assert (clientIdx not in allClients), \
@@ -71,85 +88,69 @@ def MasterProcessor(log):
 
             print "startClient", clientIdx, serverIdx, "completes."
 
-        elif cmd == "clientDisconnect":
-            '''
-            clientDisconnect(i): Client i disconnects with the server.
-            '''
+        if line[0] ==  "breakConnection":
+            id1 = int(line[1])
+            id2 = int(line[2])
+            """
+            Break the connection between a client and a server or between
+            two servers
+            """
+        if line[0] ==  "restoreConnection":
+            id1 = int(line[1])
+            id2 = int(line[2])
+            """
+            Restore the connection between a client and a server or between
+            two servers
+            """
+        if line[0] ==  "pause":
+            """
+            Pause the system and don't allow any Anti-Entropy messages to
+            propagate through the system
+            """
+        if line[0] ==  "start":
+            """
+            Resume the system and allow any Anti-Entropy messages to
+            propagate through the system
+            """
+        if line[0] ==  "stabilize":
+            """
+            Block until there are enough Anti-Entropy messages for all values to 
+            propagate through the currently connected servers. In general, the 
+            time that this function blocks for should increase linearly with the 
+            number of servers in the system.
+            """
+        if line[0] ==  "printLog":
+            serverId = int(line[1])
+            """
+            Print out a server's operation log in the format specified in the
+            handout.
+            """
+        if line[0] ==  "put":
+            clientId = int(line[1])
+            songName = line[2]
+            URL = line[3]
+            """
+            Instruct the client specified to associate the given URL with the given
+            songName. This command should block until the client communicates with
+            one server.
+            """ 
+        if line[0] ==  "get":
+            clientId = int(line[1])
+            songName = line[2]
+            """
+            Instruct the client specified to attempt to get the URL associated with
+            the given songName. The value should then be printed to standard out of 
+            the master script in the format specified in the handout. This command 
+            should block until the client communicates with one server.
+            """ 
+        if line[0] ==  "delete":
+            clientId = int(line[1])
+            songName = line[2]
+            """
+            Instruct the client to delete the given songName from the playlist. 
+            This command should block until the client communicates with one server.
+            """ 
 
-        elif cmd == "clientReconnect":
-            '''
-            clientReconnect(i,j): Client i reconnect to server j. We use this
-            cmd after client i has disconnected with server by calling
-            clientDisconnect(i).
-            '''
-
-        elif cmd == "pause":
-            '''
-            Pause the progress of the Bayou protocol, later you can continue the
-            execution of the protocol.
-            '''
-
-        elif cmd == "continue":
-            '''
-            Continue after pause.
-            '''
-
-        elif cmd == "printLog" and len(args) == 1:
-            '''
-            Print logs of all nodes.
-            '''
-
-        elif cmd == "printLog" and len(args) == 2:
-            serverIdx = args[1]
-            '''
-            Print log of node i.
-            '''
-
-        elif cmd == "Isolate":
-            serverIdx = args[1]
-            '''
-            Isolate(i): Node i is partitioned from other nodes.
-            '''
-
-        elif cmd == "reconnect":
-            serverIdx = args[1]
-            '''
-            reconnect(i): Node i is connected to all other nodes. It is used when
-            we try to recover the connections after we call partition(i).
-            '''
-
-        elif cmd == "breakConnection":
-            serverOneIdx = args[1]
-            serverTwoIdx = args[2]
-            '''
-            breakConnection(i,j): Break the connection between Node i and Node j.
-            '''
-
-
-        elif cmd == "recoverConnection":
-            serverOneIdx = args[1]
-            serverTwoIdx = args[2]
-            '''
-            recoverConnection(i,j): Recover the connection between Node i and Node j.
-            '''
-
-
-        elif cmd == "join":
-            serverIdx = int(args[1])
-            '''
-            join(i): Node i joins the system.
-            '''
-            assert (serverIdx not in allServers), \
-                    "JOIN: server %d already exists." % serverIdx
-            allServers.update([serverIdx])
-
-            print "join ", serverIdx, "completes."
-
-        elif cmd == "leave":
-            '''
-            leave(i): Node i leaves (retires from) the system.
-            '''
-            serverIdx = args[1]
     # Command processing completes
     # TODO: send messages to terminate the whole system
     for clientIdx in allClients:

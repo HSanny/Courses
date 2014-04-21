d813bd27c9dfc5abd41715061357b74311481db3
jimmylin192 <linxin@gmail.com>
2014-04-21 13:24:20 -0500

    new tests

---
+++ tests/delete.sol
@@ -0,0 +1,6 @@
+PUT:(eyesOfTexas, utexas.edu):TRUE
+PUT:(UTCS, cs.utexas.edu):TRUE
+UTCS: cs.utexas.edu
+PUT:(eyesOfTexas, utexas.edu):TRUE
+PUT:(UTCS, cs.utexas.edu):TRUE
+DELETE:(eyesOfTexas, utexas.edu):TRUE

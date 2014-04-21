7802dea68036fa357a7e0489f657a960ec4605e2
jimmylin192 <linxin@gmail.com>
2014-04-21 13:59:22 -0500

    run script and tests directory

---
+++ tests/solutions/cover.sol
@@ -0,0 +1,3 @@
+eyesOfTexas: cs.utexas.edu
+PUT:(eyesOfTexas, utexas.edu):TRUE
+PUT:(eyesOfTexas, cs.utexas.edu):TRUE
--- tests/solutions/delete.sol
+++ tests/solutions/delete.sol
@@ -3,4 +3,5 @@ PUT:(UTCS, cs.utexas.edu):TRUE
 UTCS: cs.utexas.edu
 PUT:(eyesOfTexas, utexas.edu):TRUE
 PUT:(UTCS, cs.utexas.edu):TRUE
-DELETE:(eyesOfTexas, utexas.edu):TRUE
+DELETE:(UTCS, cs.utexas.edu):TRUE
+UTCS: ERR_KEY

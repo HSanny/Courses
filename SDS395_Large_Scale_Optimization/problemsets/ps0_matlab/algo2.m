a67dc49f041d2badd951af0e5bfac0d2e5305cff
jimmylin192 <linxin@gmail.com>
2014-09-07 21:19:43 -0400

    programming assignment finished

--- SDS395_Large_Scale_Optimization/problemsets/ps0_matlab/algo2.m
+++ SDS395_Large_Scale_Optimization/problemsets/ps0_matlab/algo2.m
@@ -4,41 +4,39 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 format short e
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% solve least-square problem for first dataset
+EPSILON = 10e-5;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% LASSO least-square for Small-scale dataset
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cvx_begin
     variable b1(size(X1,2))
     minimize( norm( X1*b1-y1 ) + norm(b1,1) )
 cvx_end
-%% apply learned model on testing data
-pred1 = X1test * b1;
 
 RegressionError1 = norm( X1*b1-y1 )
-TestingError1 = norm(pred1 - y1test)
-Support1 = sum(((b1 < 10e-5) + (b1 > -10e-5)) < 2)
+TestingError1 = norm( X1test * b1 - y1test )
+Support1 = sum(((b1 < EPSILON) + (b1 > -EPSILON)) < 2)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% solve least-square problem for second dataset
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% LASSO least-square for Medium-scale dataset
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cvx_begin
     variable b2(size(X2,2))
     minimize( norm( X2*b2-y2 ) + norm(b2, 1))
 cvx_end
-%% apply learned model on testing data
-pred2 = X2test * b2;
 
 RegressionError2 = norm( X2*b2-y2 )
-TestingError2 = norm(pred2 - y2test)
-Support2 = sum(((b2 < 10e-5) + (b2 > -10e-5)) < 2)
+TestingError2 = norm( X2test * b2 - y2test )
+Support2 = sum(((b2 < EPSILON) + (b2 > -EPSILON)) < 2)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% solve least-square problem for second dataset
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% LASSO least-square for Large-scale dataset
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cvx_begin
     variable b3(size(X3,2))
     minimize( norm( X3*b3-y3 ) + norm(b3, 1) )
 cvx_end
-%% apply learned model on testing data
-pred3 = X3test * b3;
 
 RegressionError3 = norm( X3*b3-y3 )
-TestingError3 = norm(pred3 - y3test)
-Support3 = sum(((b3 < 10e-5) + (b3 > -10e-5)) < 2)
+TestingError3 = norm( X3test * b3 - y3test )
+Support3 = sum(((b3 < EPSILON) + (b3 > -EPSILON)) < 2)
